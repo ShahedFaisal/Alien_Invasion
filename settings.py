0cad0ee2e063269b39a786978b7c7c3365859ff8
ShahedFaisal <sfaisal2005@gmail.com>
2022-10-09 18:41:12 -0500

    create ship class and draw the ship

--- settings.py
+++ settings.py
@@ -3,6 +3,6 @@ class Settings:
     def __init__(self):
         """Initialize the game's settings."""
         # Screen settings
-        self.screen_width = 600
-        self.screen_height = 400
+        self.screen_width = 800
+        self.screen_height = 600
         self.bg_color = (230, 230, 230)
