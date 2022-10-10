f215d213008caaf96f7fc64547b068217292365a
ShahedFaisal <sfaisal2005@gmail.com>
2022-10-09 21:57:18 -0500

    create alien class and draw an alien

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,6 +3,8 @@ import pygame
 from settings import Settings
 from ship import Ship
 from bullet import Bullet
+from alien import Alien
+
 
 class AlienInvasion:
     """Overall class to manage game assets and behavior."""
@@ -19,6 +21,9 @@ class AlienInvasion:
 
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group()
+        self.aliens = pygame.sprite.Group()
+
+        self._create_fleet()
     
 
     def run_game(self):
@@ -78,6 +83,13 @@ class AlienInvasion:
                 self.bullets.remove(bullet)
 
 
+    def _create_fleet(self):
+        """Create the fleet of alients"""
+        # Make an alien.
+        alien = Alien(self)
+        self.aliens.add(alien)
+
+
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
         self.screen.fill(self.settings.bg_color)
@@ -87,6 +99,8 @@ class AlienInvasion:
         for bullet in self.bullets.sprites():
             bullet.draw_bullet()
         
+        self.aliens.draw(self.screen)
+
         # Make the most recently drawn screen visible.
         pygame.display.flip()
 
